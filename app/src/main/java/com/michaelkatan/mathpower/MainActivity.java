8be71c63f14ed719d2038e50e21a22c0eee9cae3
katanmichael <michaelkatan@gmail.com>
2018-01-06 17:08:51 +0200

    Added Buttons to main UI

--- app/src/main/java/com/michaelkatan/mathpower/MainActivity.java
+++ app/src/main/java/com/michaelkatan/mathpower/MainActivity.java
@@ -1,13 +1,35 @@
 package com.michaelkatan.mathpower;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
+    ActionBar bar;
+    Window window;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bar = getActionBar();
+        bar.hide();
+        window = this.getWindow();
+        changeStatusBarColor(R.color.colorPrimaryDark);
+
+
+    }
+
+    public void changeStatusBarColor(int color)
+    {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getResources().getColor(color));
+
     }
 }
