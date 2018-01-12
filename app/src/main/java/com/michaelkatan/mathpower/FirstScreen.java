469eb1e2644658c57bcf97fbd0232f4d4d0ff557
katanmichael <michaelkatan@gmail.com>
2018-01-12 23:12:04 +0200

    signup working
    Signed-off-by: katanmichael <michaelkatan@gmail.com>

--- app/src/main/java/com/michaelkatan/mathpower/FirstScreen.java
+++ app/src/main/java/com/michaelkatan/mathpower/FirstScreen.java
@@ -2,12 +2,22 @@ package com.michaelkatan.mathpower;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by MichaelKatan on 06/01/2018.
@@ -23,6 +33,10 @@ public class FirstScreen extends Activity {
     Button first_signUp;
     Button first_signIn;
 
+    String email;
+    String pass;
+
+    private FirebaseAuth myAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +48,46 @@ public class FirstScreen extends Activity {
         first_signUp = findViewById(R.id.first_signup_btn);
         first_signIn = findViewById(R.id.first_signin_btn);
 
+        myAuth = FirebaseAuth.getInstance();
+        SharedPreferences sharedPreferences = getSharedPreferences("users", MODE_PRIVATE);
 
+        first_email_et.setText(sharedPreferences.getString("email", ""));
+
+
+        first_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = first_email_et.getText().toString();
+                pass = first_pass_et.getText().toString();
+
+                if (!(email.equals("")) && !(pass.equals(""))) {
+                    myAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("Signup", "createUserWithEmail:success");
+                                FirebaseUser user = myAuth.getCurrentUser();
+
+                                Intent intent = new Intent(FirstScreen.this, MainActivity.class);
+                                intent.putExtra("email", user.getEmail());
+                                intent.putExtra("ID", user.getUid());
+
+                                startActivity(intent);
+
+                            } else {
+
+                            }
+
+
+                        }
+                    });
+
+
+                }
+
+
+            }
+        });
 
 
 
@@ -54,6 +107,18 @@ public class FirstScreen extends Activity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.setStatusBarColor(getResources().getColor(color));
         }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("users", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("email", first_email_et.getText().toString());
+        editor.commit();
 
     }
 }
