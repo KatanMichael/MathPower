2f2a81a0a80f71b6869a6174d995faa7816474fa
katanmichael <michaelkatan@gmail.com>
2018-01-17 19:41:22 +0200

    Start Game Manager Integration
    Signed-off-by: katanmichael <michaelkatan@gmail.com>

--- app/src/main/java/com/michaelkatan/PowerMath/GameManager.java
+++ app/src/main/java/com/michaelkatan/PowerMath/GameManager.java
@@ -1,7 +1,10 @@
 package com.michaelkatan.PowerMath;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 
 /**
@@ -9,9 +12,28 @@ import android.os.Bundle;
  */
 
 public class GameManager extends Activity {
+
+    Player player;
+    ArrayList<Class> levels;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        player = new Player();
+        levels = new ArrayList<>();
+
+        levels.add(AmericanQuiz.class);
+
+
+        player.set_lives(3);
+
+
+    }
+
+
+    private void startRandomLevel() {
+        Intent intent = new Intent(GameManager.this, levels.get(0));
+        startActivity(intent);
+        ;
     }
 }
